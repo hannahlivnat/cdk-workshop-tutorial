e4a82b6591ceea96daf4e20458b2342af8f80012
Hannah Livnat <hannahlivnat@gmail.com>
2020-11-02 15:57:11 -0800

    aws cdk tutorial - cdk workshop typescript

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -4,3 +4,9 @@ import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
 const app = new cdk.App();
 new CdkWorkshopStack(app, 'CdkWorkshopStack');
+
+// ===========================================================
+// This is the app's entry point. It loads and instantiates
+// CdkWorkshopStack class from lib/cdk-workshop-stack.ts file. 
+// ===========================================================
+
---
+++ lib/hitcounter.ts
@@ -0,0 +1,41 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
+export interface HitCounterProps {
+  // Function to count url hits
+  downstream: lambda.Function;
+}
+
+export class HitCounter extends cdk.Construct {
+  // Allows accessing of counter function
+  public readonly handler: lambda.Function;
+
+  // Lets hitaccess dynamodb table be public accessible
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+    this.table = table;
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'hitcounter.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
+
+    // Grant lambda role read/write permissions to table
+    table.grantReadWriteData(this.handler);
+
+    // Grant lambda role invoke permissions to downstream function
+    props.downstream.grantInvoke(this.handler);
+  }
+}
